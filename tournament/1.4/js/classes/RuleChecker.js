a3be2f47a226df9ac83d55406da4fe23c576c9c5
fliegenfuerst <fliegenfuerst@gmail.com>
2023-07-18 09:43:39 +0200

    Update RuleChecker.js

--- tournament/1.4/js/classes/RuleChecker.js
+++ tournament/1.4/js/classes/RuleChecker.js
@@ -385,11 +385,7 @@ class RuleChecker{
 		}else{
 			getEntryButton.disabled = true;
 		}
-		if(this.retStr.length == 0){
-			gui.updateRuleCheckerDiv("Your entry is valid.", this.isValid);
-		}else{
-			gui.updateRuleCheckerDiv(`<li>${this.retStr.join("</li><li>")}</li>`, this.isValid);
-		}
+		gui.updateRuleCheckerDiv(`<li>${this.retStr.join("</li><li>")}</li>`, this.isValid);
 	}
 }
 const ruleChecker = new RuleChecker();
